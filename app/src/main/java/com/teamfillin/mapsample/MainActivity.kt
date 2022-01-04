cbf88c0a3d4405d6f730eaf856788bfd418d23df
Hyun Woo Lee <l2hyunwoo@gmail.com>
2022-01-04 16:59:15 +0900

    NaverMap 활용하여 현재위치 초기화하고 위치추적 기능까지 구현

--- app/src/main/java/com/teamfillin/mapsample/MainActivity.kt
+++ app/src/main/java/com/teamfillin/mapsample/MainActivity.kt
@@ -1,23 +1,40 @@
 package com.teamfillin.mapsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.annotation.UiThread
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
-import com.naver.maps.map.MapView
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.util.FusedLocationSource
 import com.teamfillin.mapsample.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var locationSource: FusedLocationSource
+    private var naverMap: NaverMap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
         lifecycle.addObserver(MapViewLifecycleObserver(binding.mapMain, savedInstanceState))
-        binding.mapMain.getMapAsync(this)
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        binding.mapMain.getMapAsync(NaverMapProvider(locationSource, naverMap))
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            naverMap?.locationTrackingMode =
+                if (!locationSource.isActivated) LocationTrackingMode.None
+                else LocationTrackingMode.Follow
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -30,48 +47,69 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         binding.mapMain.onLowMemory()
     }
 
-    @UiThread
-    override fun onMapReady(naverMap: NaverMap) {
-        naverMap.run {
-            mapType = NaverMap.MapType.Navi
-            setLayerGroupEnabled(NaverMap.LAYER_GROUP_TRANSIT, false)
-            setLayerGroupEnabled(NaverMap.LAYER_GROUP_BUILDING, true)
-            isNightModeEnabled = true
+    private class NaverMapProvider(
+        private val trackingLocationSource: LocationSource,
+        private var activityNaverMap: NaverMap? = null
+    ) : OnMapReadyCallback {
+        override fun onMapReady(naverMap: NaverMap) {
+            activityNaverMap = naverMap.apply {
+                mapType = NaverMap.MapType.Navi
+                setLayerGroupEnabled(NaverMap.LAYER_GROUP_TRANSIT, false)
+                setLayerGroupEnabled(NaverMap.LAYER_GROUP_BUILDING, true)
+                isNightModeEnabled = true
+                locationSource = trackingLocationSource
+                locationTrackingMode = LocationTrackingMode.Follow
+                addOnLocationChangeListener { location ->
+                    cameraPosition =
+                        CameraPosition(LatLng(location.latitude, location.longitude), 16.0)
+                }
+                uiSettings.run {
+                    isCompassEnabled = false
+                    isScaleBarEnabled = false
+                    isZoomControlEnabled = false
+                    isLocationButtonEnabled = true
+                }
+            }
         }
     }
+
+    private class MapViewLifecycleObserver(
+        private val mapView: MapView,
+        private val savedInstanceState: Bundle?
+    ) : DefaultLifecycleObserver {
+        override fun onCreate(owner: LifecycleOwner) {
+            super.onCreate(owner)
+            mapView.onCreate(savedInstanceState)
+        }
+
+        override fun onDestroy(owner: LifecycleOwner) {
+            super.onDestroy(owner)
+            mapView.onDestroy()
+        }
+
+        override fun onPause(owner: LifecycleOwner) {
+            super.onPause(owner)
+            mapView.onPause()
+        }
+
+        override fun onResume(owner: LifecycleOwner) {
+            super.onResume(owner)
+            mapView.onResume()
+        }
+
+        override fun onStart(owner: LifecycleOwner) {
+            super.onStart(owner)
+            mapView.onStart()
+        }
+
+        override fun onStop(owner: LifecycleOwner) {
+            super.onStop(owner)
+            mapView.onStop()
+        }
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+    }
 }
 
-class MapViewLifecycleObserver(
-    private val mapView: MapView,
-    private val savedInstanceState: Bundle?
-) : DefaultLifecycleObserver {
-    override fun onCreate(owner: LifecycleOwner) {
-        super.onCreate(owner)
-        mapView.onCreate(savedInstanceState)
-    }
-
-    override fun onDestroy(owner: LifecycleOwner) {
-        super.onDestroy(owner)
-        mapView.onDestroy()
-    }
-
-    override fun onPause(owner: LifecycleOwner) {
-        super.onPause(owner)
-        mapView.onPause()
-    }
-
-    override fun onResume(owner: LifecycleOwner) {
-        super.onResume(owner)
-        mapView.onResume()
-    }
-
-    override fun onStart(owner: LifecycleOwner) {
-        super.onStart(owner)
-        mapView.onStart()
-    }
-
-    override fun onStop(owner: LifecycleOwner) {
-        super.onStop(owner)
-        mapView.onStop()
-    }
-}
